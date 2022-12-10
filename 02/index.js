5114017bf6672f6e0f094a1567331100b287ae09
Fred Marecesche <fred@froddd.co.uk>
2022-12-10 18:08:36 +0000

    Day 2 part 2

--- 02/index.js
+++ 02/index.js
@@ -1,5 +1,15 @@
-const gameTotal = (input) => {
+const gameTotal = (input, moves) => {
     let points = 0;
+
+    input.forEach(line => {
+        const [opponentMove, myMove] = line.split(' ');
+        points += moves[opponentMove][myMove];
+    })
+
+    return points;
+}
+
+const part1 = (input) => {
     const moves = {
         A: {
             X: 1 + 3,
@@ -17,15 +27,31 @@ const gameTotal = (input) => {
             Z: 3 + 3
         }
     }
+    return gameTotal(input, moves)
+}
 
-    input.forEach(line => {
-        const [opponentMove, myMove] = line.split(' ');
-        points += moves[opponentMove][myMove];
-    })
-
-    return points;
+const part2 = (input) => {
+    const moves = {
+        A: {
+            X: 3 + 0,
+            Y: 1 + 3,
+            Z: 2 + 6
+        },
+        B: {
+            X: 1 + 0,
+            Y: 2 + 3,
+            Z: 3 + 6
+        },
+        C: {
+            X: 2 + 0,
+            Y: 3 + 3,
+            Z: 1 + 6
+        }
+    }
+    return gameTotal(input, moves);
 }
 
 module.exports = {
-    part1: gameTotal
+    part1,
+    part2
 }
