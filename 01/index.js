bae61603ea10f20c4121bf0b491a14ea7d98436f
Fred Marecesche <fred@froddd.co.uk>
2022-12-10 17:37:47 +0000

    Day 1 part 2

--- 01/index.js
+++ 01/index.js
@@ -1,15 +1,17 @@
-const maxCalories = (input) => {
-    const elves = input.reduce((elves, calories) => {
-        if (!calories) {
-            elves.push(0)
-        } else {
-            elves[elves.length-1] += Number(calories)
-        }
-        return elves
-    }, [0])
-    return Math.max(...elves)
-}
+const addUpCalories = (input) => input.reduce((elves, calories) => {
+    if (!calories) {
+        elves.push(0)
+    } else {
+        elves[elves.length-1] += Number(calories)
+    }
+    return elves
+}, [0])
+
+const maxCalories = (input) => Math.max(...addUpCalories(input))
+
+const top3MaxCalories = (input) => addUpCalories(input).sort((a, b) => b - a).slice(0, 3).reduce((sum, x) => sum + x, 0)
 
 module.exports = {
-    part1: maxCalories
+    part1: maxCalories,
+    part2: top3MaxCalories
 }
