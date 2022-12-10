5fabae402f8891a555b968aa255dc1dbf4e6c9c0
Fred Marecesche <fred@froddd.co.uk>
2022-12-10 18:50:41 +0000

    Day 3 part 2

--- 03/index.js
+++ 03/index.js
@@ -1,3 +1,11 @@
+const getValue = character => {
+    let value = character.toLowerCase().charCodeAt(0) - 96
+    if (character.toLowerCase() !== character) {
+        value += 26
+    }
+    return value
+}
+
 const part1 = input => {
     let sumPriorities = 0
 
@@ -6,16 +14,26 @@ const part1 = input => {
         const secondCompartment = Array.from(line.slice(line.length/2))
         const [item] = firstCompartment.filter(x => secondCompartment.includes(x))
 
-        let value = item.toLowerCase().charCodeAt(0) - 96
-        if (item.toLowerCase() !== item) {
-            value += 26
-        }
-        sumPriorities += value
+        sumPriorities += getValue(item)
     })
 
     return sumPriorities
 }
-const part2 = input => {}
+const part2 = input => {
+    let sumPriorities = 0
+
+    for (let i = 0; i < input.length; i = i + 3) {
+        const line1 = Array.from(input[i])
+        const line2 = Array.from(input[i + 1])
+        const line3 = Array.from(input[i + 2])
+
+        const [item] = line1.filter(x => line2.includes(x)).filter(x => line3.includes(x))
+
+        sumPriorities += getValue(item)
+    }
+
+    return sumPriorities
+}
 
 module.exports = {
     part1,
