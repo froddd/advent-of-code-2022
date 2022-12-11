fbfd7c6fdd498f6647f6afd33a901f194fceeed3
Fred Marecesche <fred@froddd.co.uk>
2022-12-11 19:12:31 +0000

    Day 5 part 2

--- 05/index.js
+++ 05/index.js
@@ -1,7 +1,6 @@
-const part1 = input => {
+const getStacks = input => {
     const crates = input.filter(line => line.match(/\[[A-Z]]/));
     const numStacks = input.find(line => line.match(/^(\d|\s)*$/)).trim().split(/\s+/).length;
-    const moves = input.filter(line => line.startsWith('move'));
 
     const stacks = [];
     crates.reverse().forEach(line => {
@@ -14,8 +13,17 @@ const part1 = input => {
         }
     });
 
-    moves.forEach(line => {
-        const [quantity, source, destination] = line.match(/move (\d+) from (\d+) to (\d+)/).slice(1,4).map(x => parseInt(x));
+    return stacks;
+}
+
+const getMoves = input => input.filter(line => line.startsWith('move')).map(line => line.match(/move (\d+) from (\d+) to (\d+)/).slice(1,4).map(x => parseInt(x)));
+
+const part1 = input => {
+    const stacks = getStacks(input);
+    const moves = getMoves(input);
+
+    moves.forEach(move => {
+        const [quantity, source, destination] = move;
         for (let i = 1; i <= quantity; i++) {
             const crateToMove = stacks[source-1].pop();
             stacks[destination-1].push(crateToMove);
@@ -25,7 +33,18 @@ const part1 = input => {
     return stacks.map(stack => stack.pop()).join('');
 }
 
-const part2 = input => {}
+const part2 = input => {
+    const stacks = getStacks(input);
+    const moves = getMoves(input);
+
+    moves.forEach(move => {
+        const [quantity, source, destination] = move;
+        const cratesToMove = stacks[source-1].splice(-quantity, quantity);
+        stacks[destination-1].push(...cratesToMove);
+    })
+
+    return stacks.map(stack => stack.pop()).join('');
+}
 
 module.exports = {
     part1,
