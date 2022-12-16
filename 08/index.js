b7e006b92cc102dd48e1ee279e828cda7d9db7c5
Fred Marecesche <fred@froddd.co.uk>
2022-12-16 11:11:16 +0000

    Day 8 part 2

--- 08/index.js
+++ 08/index.js
@@ -1,8 +1,9 @@
-const part1 = input => {
-    const trees = [];
-    let visible = 0;
 
-    input.forEach(line => trees.push(line.split('').map(x => Number(x))));
+const getTrees = input => input.map(line => line.split('').map(x => Number(x)))
+
+const part1 = input => {
+    const trees = getTrees(input)
+    let visible = 0
 
     for (let y = 0; y < trees.length; y++) {
         const line = trees[y]
@@ -22,15 +23,58 @@ const part1 = input => {
                 || height > Math.max(...treesBeforeY) // visible from top
                 || height > Math.max(...treesAfterY) // visible from bottom
             ) {
-                visible += 1;
+                visible += 1
             }
         }
     }
 
-    return visible;
+    return visible
 }
 
 const part2 = input => {
+    const trees = getTrees(input)
+    let scenicScores = []
+
+    const directionScore = (visibleTrees, height) => {
+        const index = visibleTrees.findIndex(x => x >= height)
+
+        return index === -1 ? visibleTrees.length : index + 1
+    }
+
+    for (let y = 0; y < trees.length; y++) {
+        const line = trees[y]
+
+        for (let x = 0; x < line.length; x++) {
+            const column = trees.map(line => line[x])
+            const height = line[x]
+
+            // Ignore start and end of lines as scenic score would be 0
+            if (x !== 0 && x !== line.length -1 && y !== 0 && y !== trees.length -1){
+                const treesBeforeX = line.slice(0, x)
+                const treesAfterX = line.slice(x + 1)
+                const treesBeforeY = column.slice(0, y)
+                const treesAfterY = column.slice(y + 1)
+
+                if (
+                    height > Math.max(...treesBeforeX) // visible from left
+                    || height > Math.max(...treesAfterX) // visible from right
+                    || height > Math.max(...treesBeforeY) // visible from top
+                    || height > Math.max(...treesAfterY) // visible from bottom
+                ) {
+                    const up = directionScore(treesBeforeY.reverse(), height)
+                    const left = directionScore(treesBeforeX.reverse(), height)
+                    const down = directionScore(treesAfterY, height)
+                    const right = directionScore(treesAfterX, height)
+
+                    if (![up, left, down, right].some(el => el === 0)) {
+                        scenicScores.push(up * left * down * right)
+                    }
+                }
+            }
+        }
+    }
+
+    return Math.max(...scenicScores)
 }
 
 module.exports = {
