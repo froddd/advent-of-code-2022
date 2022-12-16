808bc211edf5f9b3b50b8fcd684c8c31937e80e9
Fred Marecesche <fred@froddd.co.uk>
2022-12-16 12:44:51 +0000

    Day 9 part 2

--- 09/index.js
+++ 09/index.js
@@ -1,3 +1,37 @@
+const moveRopeParts = (head, tail) => {
+    if (Math.abs(head[0] - tail[0]) > 1 && Math.abs(head[1] - tail[1]) > 1) {
+        // If head has moved diagonally...
+        if (head[0] > tail[0]) {
+            tail[0] += 1
+        } else {
+            tail[0] -= 1
+        }
+        if (head[1] > tail[1]) {
+            tail[1] += 1
+        } else {
+            tail[1] -= 1
+        }
+    } else if (Math.abs(head[0] - tail[0]) > 1) {
+        if (head[0] > tail[0]) {
+            tail[0] += 1
+        } else {
+            tail[0] -= 1
+        }
+        if (head[1] !== tail[1]) {
+            tail[1] = head[1]
+        }
+    } else if (Math.abs(head[1] - tail[1]) > 1) {
+        if (head[1] > tail[1]) {
+            tail[1] += 1
+        } else {
+            tail[1] -= 1
+        }
+        if (head[0] !== tail[0]) {
+            tail[0] = head[0]
+        }
+    }
+}
+
 const part1 = input => {
     const visited = []
     const head = [0,0]
@@ -17,26 +51,7 @@ const part1 = input => {
                 head[0] += 1
             }
 
-            if (Math.abs(head[0] - tail[0]) > 1) {
-                if (head[0] > tail[0]) {
-                    tail[0] += 1
-                } else {
-                    tail[0] -= 1
-                }
-                if (head[1] !== tail[1]) {
-                    tail[1] = head[1]
-                }
-            }
-            if (Math.abs(head[1] - tail[1]) > 1) {
-                if (head[1] > tail[1]) {
-                    tail[1] += 1
-                } else {
-                    tail[1] -= 1
-                }
-                if (head[0] !== tail[0]) {
-                    tail[0] = head[0]
-                }
-            }
+            moveRopeParts(head, tail)
 
             const tailCoords = `${tail[0]},${tail[1]}`
 
@@ -49,7 +64,43 @@ const part1 = input => {
     return visited.length
 }
 
-const part2 = input => {}
+const part2 = input => {
+    const visited = []
+    const rope = Array(10).fill([]).map(x => [0,0])
+
+    input.forEach(line => {
+        const [direction, distance] = line.split(' ')
+
+        for (let i = 0; i < parseInt(distance); i++) {
+            if (direction === 'U') {
+                rope[0][1] += 1
+            } else if (direction === 'D') {
+                rope[0][1] -= 1
+            } else if (direction === 'L') {
+                rope[0][0] -= 1
+            } else if (direction === 'R') {
+                rope[0][0] += 1
+            }
+
+            for (let j = 0; j < rope.length - 1; j++) {
+                const head = rope[j]
+                const tail = rope[j+1]
+
+                moveRopeParts(head, tail)
+            }
+
+            const tailCoords = `${rope[9][0]},${rope[9][1]}`
+
+            if (!visited.includes(tailCoords)) {
+                visited.push(tailCoords)
+            }
+        }
+
+
+    })
+
+    return visited.length
+}
 
 module.exports = {
     part1,
