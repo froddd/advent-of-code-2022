6dbd687630237ea65b8b9aa3bf56a05dfb1d1e60
Fred Marecesche <fred@froddd.co.uk>
2022-12-21 12:58:56 +0000

    Day 14 part 2

--- 14/index.js
+++ 14/index.js
@@ -1,36 +1,5 @@
-const getMinMax = rocks => {
-    const x = rocks.map(rock => Number(rock.split(',')[0]))
-    const y = rocks.map(rock => Number(rock.split(',')[1]))
-
-    return [Math.min(...x), Math.max(...x), 0, Math.max(...y)]
-}
-
-const render = (rocks, sands) => {
-    const [minX, maxX, minY, maxY] = getMinMax(rocks)
-
-    let output = ''
-
-    for (let i = minY; i <= maxY; i++) {
-        for (let j = minX; j <= maxX; j++) {
-            const coords = `${j},${i}`
-            if (rocks.includes(coords)) {
-                output += '#'
-            } else if (sands.includes(coords)) {
-                output += 'o'
-            } else {
-                output += '.'
-            }
-        }
-        output += '\n'
-    }
-
-    console.log(output)
-}
-
-const part1 = input => {
-    // draw the lines
+const getRocks = input => {
     const rocks = []
-    const sands = []
 
     input.forEach(line => {
         const ends = line.split('->').map(coords => coords.split(',').map(coord => Number(coord)))
@@ -48,6 +17,50 @@ const part1 = input => {
         })
     })
 
+    return rocks
+}
+
+const getMinMax = rocks => {
+    const x = rocks.map(rock => Number(rock.split(',')[0]))
+    const y = rocks.map(rock => Number(rock.split(',')[1]))
+
+    return [Math.min(...x), Math.max(...x), 0, Math.max(...y)]
+}
+
+const render = (rocks, sands, untilBlocked = false) => {
+    let [minX, maxX, minY, maxY] = getMinMax(untilBlocked ? sands : rocks)
+
+    if (untilBlocked) {
+        minX -= 2
+        maxX += 2
+    }
+
+    let output = ''
+
+    for (let i = minY; i <= maxY; i++) {
+        for (let j = minX; j <= maxX; j++) {
+            const coords = `${j},${i}`
+            if (rocks.includes(coords)) {
+                output += '#'
+            } else if (sands.includes(coords)) {
+                output += 'o'
+            } else {
+                output += '.'
+            }
+        }
+        output += '\n'
+    }
+
+    if (untilBlocked) {
+        output += Array((maxX - minX) + 1).fill('#').join('')
+    }
+
+    console.log(output)
+}
+
+const part1 = input => {
+    const rocks = getRocks(input)
+    const sands = []
     const [minX, maxX, minY, maxY] = getMinMax(rocks)
 
     let overflow = false
@@ -92,7 +105,66 @@ const part1 = input => {
     return units
 }
 
-const part2 = input => {}
+const part2 = input => {
+    const rocks = getRocks(input)
+    const sands = []
+    const maxY = Math.max(...rocks.map(rock => Number(rock.split(',')[1]))) + 1
+
+    let blocked = false
+    let units = 0
+
+    const isOccupied = coords => rocks.includes(coords.join(',')) || sands.includes(coords.join(','))
+
+    // This takes a long while to complete... But it does /shrug
+    // Uncomment stdout write to visualise ongoing progress
+    while (blocked === false) {
+        // New sand
+        let settled = false
+        let coords = [500, 0]
+
+        // if (units % 1000 === 0) {
+        //     console.log('\n')
+        //     console.log(units)
+        // }
+
+        while (settled === false) {
+            const down = [coords[0], coords[1] + 1]
+            const downLeft = [coords[0] -1, coords[1] + 1]
+            const downRight = [coords[0] +1, coords[1] + 1]
+
+            if (coords[1] >= maxY) {
+                // process.stdout.write('.')
+                settled = true
+            } else if (!isOccupied(down)) {
+                // console.log('Down is free')
+                coords = down
+            } else if (!isOccupied(downLeft)) {
+                // console.log('Down left is free')
+                coords = downLeft
+            } else if (!isOccupied(downRight)) {
+                // console.log('Down right is free')
+                coords = downRight
+            } else {
+                // process.stdout.write('o')
+                settled = true
+            }
+
+            if (settled === true) {
+                units += 1
+                sands.push(coords.join(','))
+            }
+
+            if (isOccupied([500, 0])) {
+                settled = true
+                blocked = true
+            }
+        }
+    }
+
+    // render(rocks, sands, true)
+
+    return units
+}
 
 module.exports = {
     part1,
